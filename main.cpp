6b89594d69f5171c88241035f3f72befe7f50fe3
slava <osslav@mail.com>
2022-02-28 13:36:13 +0700

    add example

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
+#include <MyStack.h>
 #include <QTextStream>
 QTextStream cout(stdout);
 
 int main() {
-    cout << "Hello" << endl;
+    double arr[6] = { 1, 2, 3, 4, 5, 6 };
+    double arr1[6] = { 11, 12, 13, 14, 15, 16 };
+    MyStack<double> test(arr, 5), test1(arr1, 6);
+    test = test1;
+
+    while (test.getCount() > 0)
+    {
+        cout << test.top() << ' ';
+        test.pop();
+    }
+    cout << endl;
 
     return 0;
 }
